2e72a93afde2774697184be48b70cb8071c47d64
Ahmad Masud <ahmadmasud25@hotmail.com>
2023-03-21 22:00:56 -0700

    Delete DrawMaven directory

--- DrawMaven/src/States.java
+++
@@ -1,8 +0,0 @@
-public class States {
-    public static final int PEN = 0;
-    public static final int ERASER = 1;
-    public static final int PAINTBUCKET = 2;
-    public static final int BRUSH = 3;
-    public static final int EYEDROPTOOL = 4;
-    public static final int PENCIL = 5;
-}
